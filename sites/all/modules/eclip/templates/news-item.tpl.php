7da9d5d00020578afe23a1ff5bf0bdcd75cf5498
Gerardo Cadau <gerardocadau@gmail.com>
2014-03-30 18:15:08 -0300

    Adding related news to new templates

--- sites/all/modules/eclip/templates/news-item.tpl.php
+++ sites/all/modules/eclip/templates/news-item.tpl.php
@@ -5,5 +5,8 @@
 	<div class="title" style="font-size: <?php print $colores['title_size'] ?>; font-weight: <?php print $colores['title_weight'] ?>"><?php print l($titulo, $link, array('absolute' => TRUE, 'attributes' => array('style' => 'color:' . $colores['link_color'] . '; text-decoration:' . $colores['title_decoration'] . '; font-family
 	:' . $colores['title_font']))) ?></div>	
 	<div class="medio-fecha" style="font-family: <?php print $colores['title_font'] ?>"><strong><?php print $medio; ?> - <?php print $fecha; ?></strong></div>
-	<div class="description" style="font-family: <?php print $colores['title_font'] ?>"><?php print $bajada ?></div>
+	<?php if(!$email): ?>
+	<div class"relacionadas"><?php print theme('news_related', array('article_id' => $article_id, 'colores' => $colores)); ?></div>
+	<?php endif; ?>
+	<div class="description" style="font-family: <?php print $colores['title_font'] ?>"><?php print $bajada ?></div>	
 </div>
--- sites/all/modules/eclip/templates/news-related.tpl.php
+++ sites/all/modules/eclip/templates/news-related.tpl.php
@@ -1,1 +1,10 @@
-hola
+<?php if (!empty($relacionadas)) : ?>
+<div class="toggle-related-items" article="<?php print $article_id; ?>">
+	<a href="#" style="color: <?php print $colores['link_color'] ?>">[Ver misma noticia en otros medios]</a>
+</div>
+<div class="related-items" article="<?php print $article_id; ?>">
+	<?php foreach ($relacionadas as $key => $relacionada) : ?>
+		<?php print $relacionada; ?>
+	<?php endforeach; ?>
+</div>
+<?php endif; ?>
