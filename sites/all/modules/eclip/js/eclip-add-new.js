9543a4105dc7a91a8c1d8fc23bd0a36ee1bb3124
Gerardo Cadau <gerardocadau@gmail.com>
2014-04-22 02:05:31 -0300

    Adding the image toggle button on news admin

--- sites/all/modules/eclip/js/eclip-add-new.js
+++ sites/all/modules/eclip/js/eclip-add-new.js
@@ -11,10 +11,10 @@
                 $this.click(function(e){
                     e.preventDefault();
                     if(!$this.hasClass('added')){
-                        var markup = '<div class="news-item new-' + $this.data('nid') + ' status-1 vote-1">'
+                        var markup = '<div class="news-item new-' + $this.data('nid') + ' status-1 vote-1 image-1">'
                             + '<span class="ui-icon ui-icon-arrowthick-2-n-s"></span><span class="news-title">' + $this.data('title')
                             + '<span class="news-medio">' + $this.data('medio') + '</span>'
-                            + '</span><span class="news-actions"><span class="ui-icon ui-vote"></span><a href="' + Drupal.settings.basePath + 'adm/news/' + $this.data('nid') + '/view">'
+                            + '</span><span class="news-actions"><span class="ui-icon ui-image"></span><span class="ui-icon ui-vote"></span><a href="' + Drupal.settings.basePath + 'adm/news/' + $this.data('nid') + '/view">'
                             + '<span class="ui-icon ui-icon-circle-zoomin"></span></a><span class="ui-icon ui-icon-circle-close"></span></span></div>';
                         $(parent.document).find('.category-' + $this.data('tid') + ' .news-content .view-content').append(markup);
                         Drupal.attachBehaviors($(parent.document).find('.category-' + $this.data('tid') + ' .news-content .view-content'));
